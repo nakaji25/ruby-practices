2d0e9d217ee448f0e0802595dea49d5533eda67c
nakaj1st <0619nakaji@gmail.com>
2025-04-22 10:13:33 +0900

    条件分岐をシンプルに変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,11 +1,11 @@
 (1..20).each do |n|
-  if (n % 3 != 0) &&  (n % 5 != 0)
-    puts n
-  elsif (n % 5 != 0) && (n % 3 == 0)
-    puts "Fizz"
-  elsif (n % 5 == 0) && (n % 3 != 0)
-      puts "Buzz"
+  if (n % 3 == 0) && (n % 5 == 0)
+    puts 'FizzBuzz'
+  elsif  n % 3 == 0
+    puts 'Fizz'
+  elsif n % 5 == 0
+    puts 'Buzz'
   else
-      puts "FizzBuzz"
+    puts n
   end
 end
