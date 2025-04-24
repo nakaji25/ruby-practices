06ff3a7b422bbac1da04f78f517ebf6cdb00db47
nakaj1st <0619nakaji@gmail.com>
2025-04-24 13:49:52 +0900

    定数名変更

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,13 +2,14 @@
 require 'date'
 require 'optparse'
 
-INDENTSPACES = 5
-STARTINDENTS = 3
+INDENT_SPACES = ' ' * 5
+START_INDENTS = ' ' * 3
 
 opt = OptionParser.new
 today = Date.today
 year = today.year
 month = today.month
+
 opt.on('-y int') { |v| year = v.to_i }
 opt.on('-m int') { |v| month = v.to_i }
 opt.parse!(ARGV)
@@ -17,11 +18,12 @@ weeks = %w[日 月 火 水 木 金 土]
 start_day = Date.new(year, month, 1)
 end_day = Date.new(year, month, -1)
 
-puts ' ' * INDENTSPACES + "#{month.to_s.rjust(2)}月 #{year.to_s.rjust(4)}"
+puts INDENT_SPACES + "#{month.to_s.rjust(2)}月 #{year.to_s.rjust(4)}"
 puts weeks.join(' ')
 (1..start_day.cwday).each do
-  print ' ' * STARTINDENTS
+  print START_INDENTS
 end
+
 (start_day..end_day).each do |date|
   print ' ' unless (date.cwday == weeks.length) || (date.day == start_day.day)
   print date.day.to_s.rjust(2)
