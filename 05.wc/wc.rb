f9ad07f801508d240629dcd8905949ff4a852633
nakaj1st <0619nakaji@gmail.com>
2025-06-10 20:15:12 +0900

    パディングのメソッド化・再代入削除

--- 05.wc/wc.rb
+++ 05.wc/wc.rb
@@ -2,62 +2,68 @@
 # frozen_string_literal: true
 
 require 'optparse'
-require 'pathname'
-require 'find'
 
-def check_opt
+def main
+  opt = parse_options
+  counts =
+    if ARGV.empty?
+      [build_counts($stdin.readlines.join)]
+    else
+      ARGV.map do |file|
+        str = File.read(file)
+        build_counts(str, ARGV)
+      end
+    end
+  display_counts(counts, opt)
+end
+
+def parse_options
   opt = OptionParser.new
   options = {}
   opt.on('-l') { |lines| options[:l] = lines }
   opt.on('-w') { |words| options[:w] = words }
   opt.on('-c') { |byte| options[:c] = byte }
   opt.parse!(ARGV)
-  options = { c: true, l: true, w: true } if options.empty?
+
+  return { c: true, l: true, w: true } if options.empty?
+
   options
 end
 
-def build_counts(str)
+def build_counts(str, path = nil)
   {
     lines: str.count("\n"),
     words: str.split(/\s+/).size,
-    characters: str.encode('UTF-8').bytesize
+    characters: str.encode('UTF-8').bytesize,
+    path: path
   }
 end
 
-def display_totals(counts, opt)
-  print total_count(counts, :lines).to_s.rjust(8) if opt[:l]
-  print total_count(counts, :words).to_s.rjust(8) if opt[:w]
-  print total_count(counts, :characters).to_s.rjust(8) if opt[:c]
-  puts ' total'
-end
-
-def total_count(counts, key)
-  counts.sum { |count| count[key] }
-end
-
 def display_counts(counts, opt)
-  counts.each_with_index do |count, index|
-    print count[:lines].to_s.rjust(8) if opt[:l]
-    print count[:words].to_s.rjust(8) if opt[:w]
-    print count[:characters].to_s.rjust(8) if opt[:c]
-    puts " #{ARGV[index] unless ARGV.empty?}"
+  counts.each do |count|
+    print generate_padding(count[:lines]) if opt[:l]
+    print generate_padding(count[:words]) if opt[:w]
+    print generate_padding(count[:characters]) if opt[:c]
+    puts count[:path].nil? ? '' : count[:path]
   end
   return if ARGV.empty?
 
   display_totals(counts, opt)
 end
 
-def check_inputs
-  opt = check_opt
-  counts = if ARGV.empty?
-             [build_counts($stdin.readlines.join)]
-           else
-             ARGV.map do |file|
-               str = File.open(file).read
-               build_counts(str)
-             end
-           end
-  display_counts(counts, opt)
+def generate_padding(str)
+  str.to_s.rjust(8)
 end
 
-check_inputs
+def display_totals(counts, opt)
+  print generate_padding(sum_count(counts, :lines)) if opt[:l]
+  print generate_padding(sum_count(counts, :words)) if opt[:w]
+  print generate_padding(sum_count(counts, :characters)) if opt[:c]
+  puts ' total'
+end
+
+def sum_count(counts, key)
+  counts.sum { |count| count[key] }
+end
+
+main
